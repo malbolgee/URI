f5680dae2b987694f0211a124b2b90cebff518aa
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-08-15 04:55:52 -0400

    Add files via upload

---
+++ 1957.c
@@ -0,0 +1,17 @@
+/*
+  @autor: Malbolge;
+  @data: 14/08/2018;
+  @nome: Converter para Hexadecimal;
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+
+  int numero;
+  scanf("%d", &numero);
+  printf("%X\n", numero);
+
+
+}
