ae3e7da9ed858c4f520008b59649cf40388940c4
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-08-15 14:06:54 -0400

    Add files via upload

---
+++ 1973.c
@@ -0,0 +1,26 @@
+/*
+	@autor: Malbolge;
+	@data: 15/08/2018;
+	@nome: Jornada nas Estrelas;
+*/
+
+#include "stdio.h"
+
+int main ()
+{
+
+	int numero, i, roubados = 0, nroubados = 0, indice = 1;
+
+
+	scanf("%d", &numero);
+	int carneiros[numero];
+
+	for (i = 0; i < numero; i++)
+		scanf("%d", &carneiros[i]);
+
+	if (carneiros[i]) != 0
+		soma = carneiros[i] - carneiros[i] + 1;
+		carneiros[i] = carneiros[i] - 1;
+
+
+}
