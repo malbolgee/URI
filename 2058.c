22d71ade30e4abdfbf3e061ac278d01cfa202577
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-08-30 16:06:00 -0400

    Add files via upload

---
+++ 2058.c
@@ -0,0 +1,19 @@
+/*
+  @autor: Malbolge;
+  @data: 30/08/2018;
+  @nome: Triângulos e Polígonos Regulares;
+*/
+
+#include <stdio.h>
+
+void main (void)
+{
+
+  int lados;
+
+  scanf("%d", &lados);
+
+  printf("%d\n", lados-2);
+
+
+}
