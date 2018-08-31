c95890348409341e53e00f8c40468ab817c47361
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-08-31 11:49:51 -0400

    Add files via upload

--- 2253.c
+++ 2253.c
@@ -1,44 +1,50 @@
+/*	
+	@autor: Malbolge;
+	@data: 31/08/2018;
+	@nome: Validador de Senhas;
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
 
-
 void main (void)
 {
 
-
-	unsigned short caracteresEspeciais = 0, tam, minuscula = 0, maiuscula = 0, tamanho = false;
-	char senha[100];
+	unsigned short caracteresEspeciais = 0, tam, minuscula = 0, maiuscula = 0, tamanho;
+	char senha[1000];
 	unsigned short i, casos = 10;
 
-	while (casos)
-	{
-		scanf("%[^\n]", senha);
-	
-		tam = strlen(senha);
 
-		if (tam >= 6 && tam <= 32)
+		while (scanf(" %[^\n]", senha) != EOF)
+		{
+	
+			tam = strlen(senha);
 			tamanho = true;
-		else
-			tamanho = false;
 
-		for (i = 0; i < tam; i++)
-			if (isupper(senha[i]) != 0)
-				maiuscula++;
-			else if (islower(senha[i]) != 0)
-				minuscula++;
-			else if (senha[i] >= 32 && senha[i] <= '/')
-				caracteresEspeciais++;
+			if (6 > tam || tam > 32)
+				tamanho = false;
+			
+			for (i = 0; i < tam; i++)
+			{
+				if (isupper(senha[i]) != 0)
+					maiuscula++;
+				else if (islower(senha[i]) != 0)
+					minuscula++;
+				else if ((senha[i] >= 32 && senha[i] <= '/') || senha[i] > 58 && senha[i] > 40)
+					caracteresEspeciais++;
+			}
 
+			if (tamanho == true && maiuscula != 0 && minuscula != 0 && caracteresEspeciais == 0)
+				printf("Senha valida.\n");
+			else
+				printf("Senha invalida.\n");
 
-		if (tamanho == true && maiuscula >= 1 && minuscula >= 1 && caracteresEspeciais != 0)
-			printf("Senha inválida!\n");
-		else
-			printf("Senha Válida!\n");
+			maiuscula = 0;
+			minuscula = 0;
+			caracteresEspeciais = 0;
+			memset(senha, 0, sizeof(senha));
 
-	casos--;
-	printf("%hu\n", casos);
-	}
-	
+		}
 }
