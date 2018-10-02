c8a8d41ab679e506db5671a9aff4bd5aa66d5057
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-10-02 18:14:43 -0400

    Matriz de Quadrados

--- 1578.c
+++ 1578.c
@@ -1,3 +1,9 @@
+/*
+	@autor: Malbolge;
+	@data: 02/10/2018;
+	@nome: Matriz de Quadrados;
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -5,63 +11,57 @@ void main ()
 {
 
 	unsigned short casos, qtsCaso = 4, ordem;
-	unsigned short linha, coluna, qtsDigitos2;
-	double maior, qtsDigitos, aux;
+	unsigned short linha, coluna, qtsDigitosAtual, k;
+	long double maior;
 
 	scanf("%hu", &casos);
-
 	while (casos--)
 	{
 
 		scanf("%hu", &ordem);
 
-		unsigned int matriz[ordem][ordem];
+		long double matriz[ordem][ordem];
 
-		maior = 0;
+		// Preenche a matriz e já faz o quadrado do termo;
 		for (linha = 0; linha < ordem; linha++)
 			for (coluna = 0; coluna < ordem; coluna++)
-				scanf("%d", &matriz[linha][coluna]);
-
-		for (linha = 0; linha < ordem; linha++)
-			for (coluna = 0; coluna < ordem; coluna++)
+			{
+				scanf("%Lf", &matriz[linha][coluna]);
 				matriz[linha][coluna] *= matriz[linha][coluna];
+			}
 
-		for (linha = 0; linha < ordem; linha++)
-			for (coluna = 0; coluna < ordem; coluna++)
-				if (matriz[linha][coluna] > maior)
-					maior = matriz[linha][coluna];
-
-		qtsDigitos = 0;
-		while (maior)
-		{
-
-			maior = maior / 10;
-			qtsDigitos++;
-
-		}
-
-		
 		printf("Quadrado da matriz #%hu:\n", qtsCaso);
 		for (linha = 0; linha < ordem; linha++)
 		{	
 			
 			for (coluna = 0; coluna < ordem; coluna++)
 			{
+				maior = 0;
+				
+				// Acha o maior elemento da coluna;
+				for (k = 0; k < ordem; k++)
+					if (matriz[k][coluna] > maior)
+						maior = matriz[k][coluna];
 
-				aux = matriz[linha][coluna];
-				qtsDigitos2 = 0;	
-				while (aux)
-				{
+				// O recuo no printf é em termos do maior elemento da coluna;
+				// Ou seja, todos os elementos da coluna estão justificados
+				// de acordo com o número que tem a maior quantidade de dígitos
+				// nessa mesma coluna;
 
-					aux = aux / 10;
-					qtsDigitos2++;
+				// log na base 10 nos da a quantidade de digitos de um número;
+				qtsDigitosAtual = ((floor(log10(maior))) + 1);
+				
+				// flag '*' antes da diretiva de formatação nos dá um recuo dinâmico
+				// Na impressão;
 
-				}
+				// Tive muitos problermas por causa disso;
+				// Não pode haver espaços após o último elemento na coluna!
+				if (coluna != ordem && coluna != 0)
+					printf(" ");
 
-				qtsDigitos2 = qtsDigitos2 - qtsDigitos;
-
-				printf("%*.Lf ", qtsDigitos, matriz[linha][coluna]);
+				printf("%*.Lf", qtsDigitosAtual, matriz[linha][coluna]);
 			}
+
 
 			printf("\n");
 
@@ -69,7 +69,9 @@ void main ()
 
 		qtsCaso++;
 
-		printf("\n");
+		// Quebra de linha condicional para não imprimir caso seja a última matriz;
+		if (casos != 0)
+			printf("\n");
 	}
 
 }
