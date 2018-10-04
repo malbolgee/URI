7bb3736e25b36f19628948f58b8a9dfd377da089
Malbolge <35477958+malbolgee@users.noreply.github.com>
2018-10-03 23:08:59 -0400

    Justificador II

--- 1278.c
+++ 1278.c
@@ -25,27 +25,33 @@ typedef struct{
 void main ()
 {
 
-	unsigned short casos, aux, tam, j, i, k;
-	unsigned char posicao;
+	unsigned short casos, aux, tam;
+	unsigned short maior, atual, i, j, k;
 
+	scanf("%hu", &casos);
 	while (true)
 	{	
-		scanf("%hu", &casos);
 
 		if (casos == 0)
 			break;
 
+		// 'aux' recebe o valor de 'casos' que será modificada no laço;
 		aux = casos;
+
+		// Vetores dos tipos 'string' e 'saida' para simular um arraylist;
 		string palavra[aux];
 		saida textoSaida[aux];
 
 		i = 0;
+		// Eche o vetor de strings com as entradas;
+		// Note que não é necessário nos preocuparmos com os espaços que podem
+		// Vir na frente da string, como não estou limpando o buffer
+		// O C descarta esses espaços, poupando o nosso trabalho;
 		while (casos--)
 			scanf(" %[^\n]", palavra[i++].palavra);
 
 		i = 0;
-		j = 0;
-		k = 0;
+		// 'tam' recebe o valor de 'aux' que será modificada no laço;
 		tam = aux;
 		while (aux--)
 		{
@@ -53,47 +59,68 @@ void main ()
 			k = 0;
 			while (true)
 			{
+				// Enquanto o caracter que estiver na posição atual for uma letra
+				// O passe para a outra struct
+				// A struct que recebe as letras, formará a nova palavra sem espaços sobressalentes;
+				while (isalpha(palavra[i].palavra[j]))	
+					textoSaida[i].saida[k++] = palavra[i].palavra[j++];
 
+				// Se o caractere que fez o laço while sair foi o caractere de final de string,
+				// Significa que a string acabou, por tanto, preciso parar o laço mais externo;
 				if (palavra[i].palavra[j] == '\0')
 					break;
 
-				while (!isalpha(palavra[i].palavra[j]))	
+				// Se não, entre nesse novo laço, porque se a string não acabou, foi porque
+				// Encontrou um espaço;
+				// Enquanto os caracteres que não forem letras forem espaços, só avança na string;
+				// Se por acaso um desses caracteres for o caractere nulo
+				// Significa que a string acabou, preciso parar o laço;
+				while (!isalpha(palavra[i].palavra[j]))
+				{	
 					j++;
+					if (palavra[i].palavra[j] == '\0')
+						break;
+				}
 
-				while (isalpha(palavra[i].palavra[j]))
-					textoSaida[i].saida[k++] = palavra[i].palavra[j++];
-
-				if (palavra[i].palavra[j] != '\0')
-					textoSaida[i].saida[k++] = ' ';
-
-				printf("textoSaida[%hu].saida[%hu]: %c\n", i, k, textoSaida[i].saida[k]);
-			}
-				
-				textoSaida[i].saida[k] = '\0';
-				i++;
-			/*
-			j--;
-			while (true)
-			{
-				
-				palavra[i].palavra[posicao++] = palavra[i].palavra[j++];
-				// printf("palavra[%hu].palavra[%hu]: %c\n", i, j, palavra[i].palavra[j]);
-
-				if (palavra[i].palavra[j] == ' ' || palavra[i].palavra[j] == '\0')
+				// Se o caractere que que fez o while acima parar foi o caractere nulo, preciso
+				// Parar o while mais externo;
+				if (palavra[i].palavra[j] == '\0')
 					break;
 
-				// printf("palavra[%hu].palavra[%hu]: %c\n", i, j, palavra[i].palavra[j]);
-				// printf("valor atual de j: %hu\n", j);
+				// Se não, coloco um espaço marcando a separação
+				// entre uma palavra e outra;
+				textoSaida[i].saida[k++] = ' ';
+
 			}
+				
+				// Caractere nulo ao final da string;
+				textoSaida[i].saida[k] = '\0';
+				i++;
 
+				
+		}	
 
-			// printf("Isso é o que tem na posicao: %c\n", palavra[i].palavra[posicao]);
-			// printf("Isso é o que tem na posicao: %c\n", palavra[i].palavra[posicao - 1]);
-			palavra[i].palavra[posicao] = '\0';
-			*/
-		}
+				maior = 0;
+				// A justificação na saída é dada em termos da maior string;
+				// Por isso, esse laço acha a maior string contida no struct 
+				// Com as strings de saída;
+				for(i = 0; i < tam; i++)
+				{
+					atual = strlen(textoSaida[i].saida);
+					if (atual > maior)
+						maior = atual;
 
-		for (i = 0; i < k; i++)
-			printf("%s\n", textoSaida[i].saida);
+				}
+
+		// Flag '*' antes da diretiva de formatação '%s'
+		// Nos fornece um recuo dinâmico;
+		for (i = 0; i < tam; i++)
+			printf("%*s\n", maior, textoSaida[i].saida);
+
+		scanf("%hu", &casos);
+
+		if(casos != 0)
+			printf("\n");
+		
 	}
 }
