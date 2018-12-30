7d7c1b4ff4da0e1edb365503c2dc0900127d2e30
Victor Hugo <victor.oliveiragomes@outlook.com>
2018-12-29 20:48:31 -0400

    As Meias de Rangel

---
+++ 2839.c
@@ -0,0 +1,19 @@
+/*
+	@autor: Malbolge;
+	@data: 29/12/2018;
+	@nome: As Meias de Rangel;
+*/
+
+#include <stdio.h>
+
+void main ()
+{
+
+	unsigned meia;
+
+	scanf("%u", &meia);
+
+	printf("%u\n", meia + 1);
+
+
+}
