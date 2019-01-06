1353519434b3775127ad9ea01cca546234488c4c
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-01-05 22:49:08 -0400

    Saída 9

---
+++ 2755.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+	@autor: Malbolge;
+	@data: 05/01/2019;
+	@nome: Saída 9;
+*/
+
+void main ()
+{
+
+	puts("\"Ro'b'er	to\\/\"");
+	puts("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+	puts("(^_-) (-_-) (-_^)");
+	puts("(\"_\") ('.')");
+
+}
