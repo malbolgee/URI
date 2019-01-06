b69a81cc8942ce46ab8fc51ccbbaf7221ff6d220
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-01-05 22:48:37 -0400

    Saída 2

---
+++ 2748.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+	@autor: Malbolge;
+	@data: 05/01/2019;
+	@nome: Saída 2;
+*/
+
+void main ()
+{
+
+	puts("---------------------------------------");
+	puts("|        Roberto                      |");
+	puts("|                                     |");
+	puts("|        5786                         |");
+	puts("|                                     |");
+	puts("|        UNIFEI                       |");
+	puts("---------------------------------------");
+
+}
