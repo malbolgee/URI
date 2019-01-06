277c9c9b77f553f57168ddd6a94f588c3cbda87c
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-01-05 22:48:56 -0400

    Saída 5

---
+++ 2751.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+	@autor: Malbolge;
+	@data: 05/01/2019;
+	@nome: Saída 5;
+*/
+
+void main ()
+{
+
+	puts("Valores de b:");
+	puts("-------------");
+	puts("1) b = 7");
+	puts("2) b =                    7");
+	puts("3) b = 00000000000000000007");
+	puts("4) b = 7                   ");
+	puts("5) b = 7%\n");
+	puts("Valores de d:");
+	puts("-------------");
+	puts("1) d = 2.208000");
+	puts("2) d = 2");
+	puts("3) d = 2.2");
+	puts("4) d = 2.21");
+	puts("5) d = 2.208");
+	puts("6) d =                2.208");
+	puts("7) d = 0000000000000002.208");
+	puts("8) d = 2.208               ");
+	puts("9) d = 2.21%");
+
+}
