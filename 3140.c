40f3347b7d74c267a65f896f4ab18b4fde945e16
victor hugo <victor.gomes@icomp.ufam.edu.br>
2020-11-02 13:38:16 -0400

    Copiando e Colando Código

---
+++ 3140.c
@@ -0,0 +1,35 @@
+/*
+    @author: Malbolge;
+    @date: 02/11/2020;
+    @name: Copiando e Colando Código;
+*/
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(int argc, char *argv)
+{
+
+    char string[1100];
+
+    bool body = false;
+    while (~scanf("%[^\n]%*c", string))
+    {
+
+        if (strstr(string, "<body>"))
+        {
+
+            body = true;
+            continue;
+        }
+
+        if (body && strstr(string, "</body>"))
+            body = false;
+
+        if (body)
+            puts(string);
+    }
+
+    return 0;
+}
